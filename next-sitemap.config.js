6bb5b6e497dd4806cc4ecbec207217b5511ab9a9
stefansav0 <ravikumar.kumar2333@gmail.com>
2025-11-19 17:52:35 +0530

    fixed server sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,48 +1,41 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://finderight.com",
+    siteUrl: 'https://finderight.com',
     generateRobotsTxt: true,
     generateIndexSitemap: true,
     sitemapSize: 5000,
-    changefreq: "daily",
-    priority: 0.7,
 
-    // ❌ Pages you do NOT want indexed
     exclude: [
-        "/admin/*",
-        "/dashboard/*",
-        "/profile",
-        "/login",
-        "/signup",
-        "/search",
-        "/server-sitemap.xml", // prevent duplication
+        '/admin/*',
+        '/dashboard/*',
+        '/profile',
+        '/login',
+        '/signup',
+        '/search',
+        '/server-sitemap.xml',
     ],
 
-    // ✔ Include custom dynamic sitemap
     additionalSitemaps: [
-        "https://finderight.com/server-sitemap.xml",
+        'https://finderight.com/server-sitemap.xml',
     ],
 
     robotsTxtOptions: {
         policies: [
             {
-                userAgent: "*",
-                allow: "/",
+                userAgent: '*',
+                allow: '/',
                 disallow: [
-                    "/admin",
-                    "/dashboard",
-                    "/profile",
-                    "/login",
-                    "/signup",
-                    "/search",
+                    '/admin',
+                    '/dashboard',
+                    '/profile',
+                    '/login',
+                    '/signup',
+                    '/search',
                 ],
             },
         ],
-
-        // ✔ Add sitemaps inside robots.txt
         additionalSitemaps: [
-            "https://finderight.com/sitemap.xml",
-            "https://finderight.com/server-sitemap.xml",
+            'https://finderight.com/server-sitemap.xml',
         ],
     },
 };
