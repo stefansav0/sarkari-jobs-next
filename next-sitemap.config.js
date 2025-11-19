3b728bc00a5565c493700e2f13ef0a9071e5db72
stefansav0 <ravikumar.kumar2333@gmail.com>
2025-11-19 17:45:38 +0530

    Updated server sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,46 +1,48 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://finderight.com',
+    siteUrl: "https://finderight.com",
     generateRobotsTxt: true,
     generateIndexSitemap: true,
     sitemapSize: 5000,
-    changefreq: 'daily',
+    changefreq: "daily",
     priority: 0.7,
 
-    // Pages Next should NOT include in sitemap
+    // ❌ Pages you do NOT want indexed
     exclude: [
-        '/admin/*',
-        '/dashboard/*',
-        '/profile',
-        '/login',
-        '/signup',
-        '/search',
-        '/server-sitemap.xml', // avoid duplication
+        "/admin/*",
+        "/dashboard/*",
+        "/profile",
+        "/login",
+        "/signup",
+        "/search",
+        "/server-sitemap.xml", // prevent duplication
     ],
 
-    // Extra dynamic sitemap created manually
+    // ✔ Include custom dynamic sitemap
     additionalSitemaps: [
-        'https://finderight.com/server-sitemap.xml',
+        "https://finderight.com/server-sitemap.xml",
     ],
 
     robotsTxtOptions: {
         policies: [
             {
-                userAgent: '*',
-                allow: '/',
+                userAgent: "*",
+                allow: "/",
                 disallow: [
-                    '/admin',
-                    '/dashboard',
-                    '/profile',
-                    '/login',
-                    '/signup',
-                    '/search',
+                    "/admin",
+                    "/dashboard",
+                    "/profile",
+                    "/login",
+                    "/signup",
+                    "/search",
                 ],
             },
         ],
+
+        // ✔ Add sitemaps inside robots.txt
         additionalSitemaps: [
-            'https://finderight.com/sitemap.xml',
-            'https://finderight.com/server-sitemap.xml',
+            "https://finderight.com/sitemap.xml",
+            "https://finderight.com/server-sitemap.xml",
         ],
     },
 };
